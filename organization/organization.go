d46de7ff919ac07a233de01b079eba7e015006ff
Raulino Neto <raulinoneto@users.noreply.github.com>
2021-06-19 06:20:31 -0300

    Merge pull request #6
    * added dinamodbav
    
    * Removed interfaces and added omitempty to @type field
    
    * solved conflicts

--- organization/organization.go
+++ organization/organization.go
@@ -6,21 +6,17 @@ import (
 )
 
 type FeatureAccessibility struct {
-	Type                 string                   `json:"@type"`
-	Description          *common.Description      `json:"Description"` // Assigned Type: ota2-0500:Description
-	ExtensionPointShared interface{}              `json:"ExtensionPoint_Shared"`
+	Type                 string                   `json:"@type,omitempty"`
+	Description          *common.Description      `json:"Description"`          // Assigned Type: ota2-0500:Description
 	Proximity            *otamodel.EnumStringBase `json:"Proximity"`            // Assigned Type: ota2-0400:Proximity_Enum
 	Quantity             int64                    `json:"quantity,omitempty"`   // Assigned Type: ota2-0500:NonNegativeInteger
 	AccessibilityFeature *otamodel.EnumStringBase `json:"AccessibilityFeature"` // Assigned Type: ota2-0400:DisabilityFeature_Enum
-	ExtensionPoint       interface{}              `json:"ExtensionPoint"`
 }
 
 type FeatureSecurity struct {
-	Type                 string                   `json:"@type"`
-	Description          *common.Description      `json:"Description"` // Assigned Type: ota2-0500:Description
-	ExtensionPointShared interface{}              `json:"ExtensionPoint_Shared"`
-	Proximity            *otamodel.EnumStringBase `json:"Proximity"`          // Assigned Type: ota2-0400:Proximity_Enum
-	Quantity             int64                    `json:"quantity,omitempty"` // Assigned Type: ota2-0500:NonNegativeInteger
-	ExtensionPoint       interface{}              `json:"ExtensionPoint"`
-	SecurityFeature      *otamodel.EnumStringBase `json:"SecurityFeature"` // Assigned Type: ota2-0400:SecurityFeature_Enum
+	Type            string                   `json:"@type,omitempty"`
+	Description     *common.Description      `json:"Description"`        // Assigned Type: ota2-0500:Description
+	Proximity       *otamodel.EnumStringBase `json:"Proximity"`          // Assigned Type: ota2-0400:Proximity_Enum
+	Quantity        int64                    `json:"quantity,omitempty"` // Assigned Type: ota2-0500:NonNegativeInteger
+	SecurityFeature *otamodel.EnumStringBase `json:"SecurityFeature"`    // Assigned Type: ota2-0400:SecurityFeature_Enum
 }
--- rooms/room.go
+++ rooms/room.go
@@ -8,7 +8,7 @@ import (
 
 // RoomTypeID Assigned Type: ph-0200:RoomType
 type RoomTypeID struct {
-	Type  string `json:"@type"`
+	Type  string `json:"@type,omitempty"`
 	ObjID string `json:"objID,omitempty" dynamodbav:"objID,omitempty"` // A unique identifier within this document for this object.
 }
 
@@ -18,7 +18,6 @@ type RoomType struct {
 	Amenity             []common.Amenity           `json:"Amenity,omitempty"`
 	Code                *common.Code               `json:"Code"`        // Assigned Type: ota2-0500:Code
 	Description         *common.Description        `json:"Description"` // Assigned Type: ota2-0500:Description
-	ExtensionPoint      interface{}                `json:"ExtensionPoint"`
 	RoomAmenities       []common.Code              `json:"RoomAmenities,omitempty"`
 	RoomCharacteristics *RoomCharacteristicsDetail `json:"RoomCharacteristics"` // Assigned Type: ph-0200:RoomCharacteristics
 	RoomOccupancy       []RoomOccupancy            `json:"RoomOccupancy,omitempty"`
@@ -26,9 +25,8 @@ type RoomType struct {
 }
 
 type RoomOccupancy struct {
-	Type              string                    `json:"@type"`
+	Type              string                    `json:"@type,omitempty"`
 	AgeQualifying     []otamodel.EnumStringBase `json:"AgeQualifying,omitempty"`
-	ExtensionPoint    interface{}               `json:"ExtensionPoint"`
 	MaxOccupancy      int64                     `json:"maxOccupancy,omitempty"`      // Assigned Type: ota2-0500:NonNegativeInteger
 	MinOccupancy      int64                     `json:"minOccupancy,omitempty"`      // Assigned Type: ota2-0500:NonNegativeInteger
 	StandardOccupancy int64                     `json:"standardOccupancy,omitempty"` // Assigned Type: ota2-0500:NonNegativeInteger
@@ -36,31 +34,29 @@ type RoomOccupancy struct {
 
 // RoomCharacteristics Assigned Type: ph-0200:RoomCharacteristics
 type RoomCharacteristics struct {
-	Type                  string                              `json:"@type"`
-	BedTypeCode           *otamodel.EnumStringBase            `json:"BedTypeCode"`            // Assigned Type: ota2-0400:BedType_Enum
-	Category              *otamodel.EnumStringBase            `json:"Category"`               // Assigned Type: ota2-0400:SegmentCategory_Enum
-	ClassificationCode    *otamodel.EnumStringBase            `json:"ClassificationCode"`     // Assigned Type: ota2-0400:GuestRoomInfo_Enum
-	CompositeInd          bool                                `json:"compositeInd,omitempty"` // Indicates that the room (suite) is a composite of smaller units.
-	ExtensionPointSummary interface{}                         `json:"ExtensionPoint_Summary"`
-	FeatureAccessibility  []organization.FeatureAccessibility `json:"FeatureAccessibility,omitempty"`
-	FeatureSecurity       []organization.FeatureSecurity      `json:"FeatureSecurity,omitempty"`
-	LocationCode          *otamodel.EnumStringBase            `json:"LocationCode"`            // Assigned Type: ota2-0400:RoomLocation_Enum
-	NonSmokingInd         bool                                `json:"nonSmokingInd,omitempty"` // Non-smoking indicator.
-	ViewCode              *otamodel.EnumStringBase            `json:"ViewCode"`                // Assigned Type: ota2-0400:RoomViewType_Enum
+	Type                 string                              `json:"@type,omitempty"`
+	BedTypeCode          *otamodel.EnumStringBase            `json:"BedTypeCode"`            // Assigned Type: ota2-0400:BedType_Enum
+	Category             *otamodel.EnumStringBase            `json:"Category"`               // Assigned Type: ota2-0400:SegmentCategory_Enum
+	ClassificationCode   *otamodel.EnumStringBase            `json:"ClassificationCode"`     // Assigned Type: ota2-0400:GuestRoomInfo_Enum
+	CompositeInd         bool                                `json:"compositeInd,omitempty"` // Indicates that the room (suite) is a composite of smaller units.
+	FeatureAccessibility []organization.FeatureAccessibility `json:"FeatureAccessibility,omitempty"`
+	FeatureSecurity      []organization.FeatureSecurity      `json:"FeatureSecurity,omitempty"`
+	LocationCode         *otamodel.EnumStringBase            `json:"LocationCode"`            // Assigned Type: ota2-0400:RoomLocation_Enum
+	NonSmokingInd        bool                                `json:"nonSmokingInd,omitempty"` // Non-smoking indicator.
+	ViewCode             *otamodel.EnumStringBase            `json:"ViewCode"`                // Assigned Type: ota2-0400:RoomViewType_Enum
 }
 
 // RoomCharacteristicsDetail Assigned Type: ph-0200:RoomCharacteristics
 type RoomCharacteristicsDetail struct {
 	RoomCharacteristics
-	ArchitectureCode     string                   `json:"architectureCode,omitempty"` // Assigned Type: ota2-0500:OTA_Code
-	ExtensionPointDetail interface{}              `json:"ExtensionPoint_Detail"`
-	Floor                int64                    `json:"floor,omitempty"`             // Floor on which the room is located.
-	Gender               *otamodel.EnumStringBase `json:"gender,omitempty"`            // Used to request or specify a gender assignment for a room. Note: Typically used by; Hosteliers.
-	InvBlockCode         string                   `json:"invBlockCode,omitempty"`      // Assigned Type: ota2-0500:StringTiny
-	NumberOfBathrooms    int64                    `json:"numberOfBathrooms,omitempty"` // The number of bathrooms in the unit.
-	NumberOfBedrooms     int64                    `json:"numberOfBedrooms,omitempty"`  // The number of bedrooms in the unit.
-	SharedRoomInd        bool                     `json:"sharedRoomInd,omitempty"`     // If TRUE, the room requires or has sharing available. Note: Typically used by Hosteliers.
-	SizeMeasurement      string                   `json:"sizeMeasurement,omitempty"`   // Textual description of room dimensions.
-	TypeCode             string                   `json:"typeCode,omitempty"`          // Assigned Type: ota2-0500:StringTiny
-	UnitNumber           string                   `json:"unitNumber,omitempty"`        // Assigned Type: ota2-0500:StringTiny
+	ArchitectureCode  string                   `json:"architectureCode,omitempty"`  // Assigned Type: ota2-0500:OTA_Code
+	Floor             int64                    `json:"floor,omitempty"`             // Floor on which the room is located.
+	Gender            *otamodel.EnumStringBase `json:"gender,omitempty"`            // Used to request or specify a gender assignment for a room. Note: Typically used by; Hosteliers.
+	InvBlockCode      string                   `json:"invBlockCode,omitempty"`      // Assigned Type: ota2-0500:StringTiny
+	NumberOfBathrooms int64                    `json:"numberOfBathrooms,omitempty"` // The number of bathrooms in the unit.
+	NumberOfBedrooms  int64                    `json:"numberOfBedrooms,omitempty"`  // The number of bedrooms in the unit.
+	SharedRoomInd     bool                     `json:"sharedRoomInd,omitempty"`     // If TRUE, the room requires or has sharing available. Note: Typically used by Hosteliers.
+	SizeMeasurement   string                   `json:"sizeMeasurement,omitempty"`   // Textual description of room dimensions.
+	TypeCode          string                   `json:"typeCode,omitempty"`          // Assigned Type: ota2-0500:StringTiny
+	UnitNumber        string                   `json:"unitNumber,omitempty"`        // Assigned Type: ota2-0500:StringTiny
 }
